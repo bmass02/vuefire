70fc8a3ddbb0b333bc228867ab1c2153379eaf45
Bryan Massoth <btmassoth@gmail.com>
2018-08-03 11:02:29 -0400

    Improperly overloaded binding functions. Added firebase as dev dependency.

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -4,9 +4,9 @@ import Vue from 'vue';
 declare module "vue/types/vue" {
   interface Vue {
     $firebase: typeof firebase
-    $bindAsObject: (key: string, source: firebase.firestore.DocumentReference) => Promise<firebase.firestore.DocumentSnapshot>;
-    $bindAsObject: (key: string, source: firebase.database.Reference) => Promise<firebase.database.DataSnapshot>;
-    $bindAsArray: (key: string, source: firebase.firestore.CollectionReference | firebase.firestore.Query) => Promise<firebase.firestore.QuerySnapshot>;
-    $bindAsArray: (key: string, source: firebase.database.Reference) => Promise<firebase.database.DataSnapshot>;
+    $bindAsObject(key: string, source: firebase.firestore.DocumentReference): Promise<firebase.firestore.DocumentSnapshot>;
+    $bindAsObject(key: string, source: firebase.database.Reference): Promise<firebase.database.DataSnapshot>;
+    $bindAsArray(key: string, source: firebase.firestore.CollectionReference | firebase.firestore.Query): Promise<firebase.firestore.QuerySnapshot>;
+    $bindAsArray(key: string, source: firebase.database.Reference): Promise<firebase.database.DataSnapshot>;
   }
 }
